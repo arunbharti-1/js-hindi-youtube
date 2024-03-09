8d6eaef9d6df8c4e78adb62f1a8449122a871c3d
arunbharti-1 <114228660+arunbharti-1@users.noreply.github.com>
2024-03-09 07:34:16 +0000

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,21 @@
+let vlaue = 3
+let negValue = -vlaue
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+// let str1 = "Hello"
+// let str2 = "Arun"
+// let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"3")
+
+console.log(true)
